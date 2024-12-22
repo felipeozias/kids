b1e01981393ffadbb6127592e37d41095745f5e2
felipeozias <95044200+felipeozias@users.noreply.github.com>
2024-12-22 13:58:44 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#242424]">
+        <h1 className="text-xl"> Aplicação React + Typescript</h1 >
+      </div>
+    </>
+  )
+}
+
+export default App
